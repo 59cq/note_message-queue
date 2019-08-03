bf9ab983a39613e4d54dea1035efaa18d29e7d74
BroWuG <38723278+BroWuGou@users.noreply.github.com>
2019-08-03 17:19:37 +0800

    Create receive.c
    接收程序

---
+++ receive.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <stdlib.h>
+#include <sreing.h>
+#include <sts/msg.h>
+#include "struct.h"
+int main(){
+    //创建消息队列
+    int msgid = msgget(ftok(".",119),IPC_CREAT|IPC_EXCL|0644);
+    if(0 > msgid){
+        perror(msgget);
+        return -1;
+    }
+    Msg msg = {666};
+    for(;;){
+        printf(">");
+        gets(msg.data);
+        msgsnd(msgid,&msg,sizeof(Msg)-sizeof(msg.type),0);
+        if(0 == strcmp("quit",msg.data)){
+            printf("通信结束\n");
+            break;
+        } 
+
+    }
+    if(0 > msgctl(msgid,IPC_RMID,NULL)){
+        perror("msgctl");
+        return -1;
+    }
+    Msg msg = {};
+    for(;;){
+        msgrcv(msgid,&msg,sizeof(Msg),555,0);
+        printf("read:%s\n",msg.data);
+        if(0 == strcmp("quit",msg.data)){
+            printf("通信结束\n");
+            break;
+        }
+    }
+}
+
+
+
